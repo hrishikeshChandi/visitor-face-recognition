125607200ca41931c727195eb04d5629d74b8654
Hrishikesh Chandi <143275248+hrishikeshChandi@users.noreply.github.com>
2025-08-24 13:43:56 +0530

    Create generate_embeddings.py

---
+++ generate_embeddings.py
@@ -0,0 +1,39 @@
+import os
+from deepface import DeepFace
+import numpy as np
+from tqdm.auto import tqdm
+import faiss
+from config import IMAGES_FOLDER, INDEX_PATH, EMBEDDINGS_PATH
+
+MODEL_NAME = "Facenet"
+DETECTOR_BACKEND = "retinaface"
+
+image_paths = [
+    f for f in os.listdir(IMAGES_FOLDER) if f.lower().endswith(("jpg", "jpeg", "png"))
+]
+
+image_paths.sort(key=lambda x: int(x.split(".")[0]))
+
+image_paths = [f"{IMAGES_FOLDER}/{f}" for f in image_paths]
+
+print("\n" * 100)
+
+embeddings = []
+for img_path in tqdm(image_paths, desc="Generating embeddings", total=len(image_paths)):
+    emb = DeepFace.represent(
+        img_path=img_path,
+        enforce_detection=True,
+        model_name=MODEL_NAME,
+        detector_backend=DETECTOR_BACKEND,
+    )
+    embeddings.append(emb[0]["embedding"])
+
+embeddings_np = np.array(embeddings, dtype=np.float32)
+
+DIMENSIONS = embeddings_np.shape[1]
+index = faiss.IndexHNSWFlat(DIMENSIONS, 32)
+index.add(embeddings_np)
+
+faiss.write_index(index, INDEX_PATH)
+np.save(EMBEDDINGS_PATH, embeddings_np)
+
